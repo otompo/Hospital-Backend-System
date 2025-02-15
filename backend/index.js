7b3255ceb38e6e27d7ffd3a7e068092d3f5b7101
otompo <abagnasalifusasco@gmail.com>
2025-02-15 08:57:49 +0000

    Size

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middlewares
 app.use(cors("*"));
-app.use(express.json({ limit: "60mb" }));
+app.use(express.json({ limit: "70mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
